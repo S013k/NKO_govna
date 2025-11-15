4f8c3e44baf9e5c28c4f52958ba57fce7923ba39
Aleksandr Sofronov <sofronov-ae@qavm-f9b691f5.qemu>
2025-11-15 14:58:40 +0300

    change order

--- database/event.sql
+++ database/event.sql
@@ -4,14 +4,6 @@ CREATE TABLE IF NOT EXISTS events_categories (
     created_at TIMESTAMPTZ DEFAULT now()
 );
 
-CREATE TABLE IF NOT EXISTS events_categories_link (
-    events_id BIGINT NOT NULL,
-    category_id SMALLINT NOT NULL,
-    PRIMARY KEY (events_id, category_id),
-    FOREIGN KEY (events_id) REFERENCES events(id) ON DELETE CASCADE,
-    FOREIGN KEY (category_id) REFERENCES events_categories(id) ON DELETE RESTRICT
-);
-
 CREATE TABLE IF NOT EXISTS events (
     id BIGSERIAL PRIMARY KEY,
     nko_id BIGINT NOT NULL REFERENCES nko(id) ON DELETE CASCADE,
@@ -23,4 +15,12 @@ CREATE TABLE IF NOT EXISTS events (
     starts_at TIMESTAMPTZ,
     finish_at TIMESTAMPTZ,
     created_at TIMESTAMPTZ DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS events_categories_link (
+    events_id BIGINT NOT NULL,
+    category_id SMALLINT NOT NULL,
+    PRIMARY KEY (events_id, category_id),
+    FOREIGN KEY (events_id) REFERENCES events(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES events_categories(id) ON DELETE RESTRICT
 );
